537d0e416ee347fef0e0f38b08d276ab654e9157
emilywong1996 <emily.wong.cohort229@gmail.com>
2023-04-29 18:57:33 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// Note: DO NOT CLONE THE PACKAGE FROM REACT QUERY TUTORIAL https://www.youtube.com/watch?v=Nm0inP3B_zs&list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2&index=2
+// 1. USE npx create-react-app react-query-demo TO MAKE YOUR FILES WITH YOUR CURRENT NODE.JS
+// 2. OOOOOOOOOOONLY COPY AND PASTE WITH WHAT YOU NEED FROM THE REPO AFTER YOU CREATE YOUR FILE WITH npx create-react-app react-query-demo, DO NOT CLONE CLONE CLONE
+// 3. COPY AND PASTE CODE FROM https://github.com/gopinav/React-Query-Tutorials/blob/master/react-query-starter/src/App.js
+// 4. ALWAYS MAKE A NEW TERMINAL AFTER yarn serve-json, DO NOT TERMINATE THE serve-json. AFTER MAKING NEW TERMINAL DO yarn start
+// 5. ALWAYS HAVE THE db.json RUNNING ON LOCAL HOST BEFORE YOU RUN PROJECT LOCAL HOST
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import './App.css'
+import { HomePage } from './components/Home.page'
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
+import { SuperHeroesPage } from './components/SuperHeroes.page'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroesPage/>}/>
+            <Route path='/rq-super-heroes' element={<RQSuperHeroesPage/>}/>
+            <Route path='/' element={<HomePage/>}/>
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
+export const RQSuperHeroesPage = () => {
+
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data)
+  }
+
+  const onError = (error) => {
+    console.log('Perform side effect after data encountering error', error)
+  }
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes,
+    {
+      onSuccess: onSuccess,
+      onError: onError
+    }
+  )
+
+  console.log({ isLoading, isFetching })
+  
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>
+  }
+
+  if(isError) {
+    return <h2>{error.message}</h2>
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch heroes</button>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>
+      })}
+    </>
+  )
+}
